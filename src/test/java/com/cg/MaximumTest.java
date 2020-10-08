801cea5159bf06c5dd3713047c65866486e755a0
krove <krove@DESKTOP-Q5SED1R>
2020-10-09 02:16:31 +0530

    [Kashif]Added:Finding Maximum of 3 Integers

--- src/test/java/com/cg/MaximumTest.java
+++ src/test/java/com/cg/MaximumTest.java
@@ -5,10 +5,21 @@ package com.cg;
 
 import org.junit.Test;
 
-import com.cg.Maximum;
-
 import static org.junit.Assert.*;
 
 public class MaximumTest {
-	
+	Maximum max =new Maximum();
+
+	@Test
+	public void testCase1_1() {
+		assertEquals((Integer)9,(Integer)max.findMaxInt(9, 2, 4));
+	}	
+	@Test
+	public void testCase1_2() {
+		assertEquals((Integer)20,(Integer)max.findMaxInt(9, 20, 4));
+	}
+	@Test
+	public void testCase1_3() {
+		assertEquals((Integer)30,(Integer)max.findMaxInt(9, 2, 30));
+	}
 }
